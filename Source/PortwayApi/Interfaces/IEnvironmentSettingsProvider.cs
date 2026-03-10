6436c2d9675fbd722bef7f9612c6257179bca36e
hawkinslabdev <59891413+hawkinslabdev@users.noreply.github.com>
2026-03-10 16:39:07 +0100

    fix: encrypt connection string after app start

--- Source/PortwayApi/Interfaces/IEnvironmentSettingsProvider.cs
+++ Source/PortwayApi/Interfaces/IEnvironmentSettingsProvider.cs
@@ -3,4 +3,5 @@ namespace PortwayApi.Interfaces;
 public interface IEnvironmentSettingsProvider
 {
     Task<(string ConnectionString, string ServerName, Dictionary<string, string> Headers)> LoadEnvironmentOrThrowAsync(string env);
+    void EncryptEnvironmentIfNeeded(string envName);
 }
